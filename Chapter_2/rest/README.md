86f823d2a8ecff7de3b33cbeffa925341aedb83b
Lenin Mookiah <leninmookiah@Lenins-MacBook-Pro.local>
2021-12-18 22:53:38 -0600

    REST API json code improved for clean read

--- Chapter_2/rest/README.md
+++ Chapter_2/rest/README.md
@@ -3,9 +3,12 @@
 - A copy of the locally stored JSON reddit file collected using REST API request
 is located [here](./reddit.json)
 
-- [get_rest_api_data.py](./get_rest_api_data.py) is used to make python request to
+- [get_rest_api_data.py](./get_rest_api_data.py) is used to make python library `urllib` request object `request` to
 REST endpoint [here](https://www.reddit.com/r/all.json) that returns JSON file. The JSON
 data is then saved as JSON object using `json.loads()` from `json` library and then
 the parsed further. The parsed elements such as author_fullname, selftext,  is_video, title, upvote_ratio, 
 is_original_content, media_embed are saved into the CSV file [reddit.csv](./reddit.csv)
-  
+
+- Notes for [get_rest_api_data.py](./get_rest_api_data.py)
+
+     -  is_read_local_json - if set True, read from locally stored json file. if False, call REST JSON endpoint to get data
--- Chapter_2/rest/get_rest_api_data.py
+++ Chapter_2/rest/get_rest_api_data.py
@@ -1,16 +1,15 @@
 # This script collects the API data from Reddit
-import requests
-import urllib.request as request
-import traceback
-import pandas as pd
-import re
 import json
+import re
+import traceback
+import urllib.request as request
+import pandas as pd
 
 # if set True, read from locally stored json file. if False, call REST JSON endpoint to get data
 is_read_local_json = True
 
 
-def json_to_csv(url, out_file):
+def json_to_csv(url, in_file, out_file):
     try:
         # file for output csv
         f_out = open(out_file, "w")
@@ -21,7 +20,7 @@ def json_to_csv(url, out_file):
         if is_read_local_json:
             print("Read from locally saved JSON file")
             # open JSON file for reading
-            f = open("./reddit.json", "r")
+            f = open(in_file, "r")
             json_data = json.load(f)
         else:
             print("Read from REST endpoint of Reddit")
@@ -40,13 +39,22 @@ def json_to_csv(url, out_file):
             data_obj = i["data"]
             # print(i)
             author = remove_comma(data_obj["author_fullname"])
+            # feature "selftext" -> text data. Needs a set of processing such as remove comma, remove
+            # non-alphanumeric characters, convert to lower case
             text = remove_comma(data_obj["selftext"])
             # replace new line in the text file. Otherwise the line will split into multiple in the output CSV file.
             text = text.replace("\n", "")
             # remove non alpha-numeric characters
             text = clean_text(text)
-            is_video = remove_comma(data_obj["is_video"])
+            # convert string to lower case
+            text = convert_lowercase(text)
+            # "title" -> needs set of cleaning similar to one did for the feature "text"
             title = remove_comma(data_obj["title"])
+            # remove non alpha-numeric characters
+            title = clean_text(title)
+            # convert string to lower case
+            title = convert_lowercase(title)
+            is_video = remove_comma(data_obj["is_video"])
             upvote_ratio = remove_comma(data_obj["upvote_ratio"])
             is_original = remove_comma(data_obj["is_original_content"])
             media_embed = remove_comma(data_obj["media_embed"])
@@ -73,20 +81,28 @@ def clean_text(in_str):
     clean_txt = re.sub(r'\W+', ' ', in_str)
     return clean_txt
 
+
 def convert_lowercase(in_str):
-    return str(in_str)
+    """ Convert a given string to lower case
+    :param in_str:
+    :return:
+    """
+    return str(in_str).lower()
+
 
 if __name__ == "__main__":
-    # URL that needs to be crawled for JSON object. Should return jSON
+    # URL that needs to be crawled for JSON data object. Should return jSON
     url = 'https://www.reddit.com/r/all.json'
+    # if the read from REST endpoint for JSON fails due to too many tries, read from already saved JSON file below
+    in_file = "./reddit.json"
     # output file
     out_file = "./reddit.csv"
     # collect Reddit JSON data and save as csv
-    json_to_csv(url, out_file)
+    json_to_csv(url, in_file, out_file)
 
-#########
+############################################################################################################
 # NOTES:
-#########
+############################################################################################################
 # Text before and after realing with regex library "re"
 # BEFORE -> As the title says I dispatch for 911. Last night I got a call for a domestic disturbance. The woman calling was so scared. She kept saying "he's gonna kill me he's gonna fucking kill me". I went through my protocol and tried to keep her calm. Dispatchers have a system called ProQA that gives guidance on what to do in certain situations. I followed that and had the police and Ambulance en route as soon as possible. I tried to tell her to get to a safe place and kept assuring her help was on the way. She said she had locked herself in the bathroom. I could hear him pounding on the door yelling "you're dead. I'm gonna fucking kill you". I did everything I could. I tried to keep her calm and help her but she was trapped. I told her I was here for her and she wasn't alone. She told me about her child a 4 year old girl and that she wasn't going to leave her. Time seemed to drag on. I checked with my officers who were only 2 minutes out and I felt some relief. I just knew she was going to be okay. Until I heard the sound of the door being kicked down. All she said was "God help me" and the most haunting scream I have ever heard and dropped the phone. 3 shots followed and I never heard from her again. My crews arrived on scene and the husband was detained. She was dead. One shot to the head two to the chest. I cannot stop replaying our conversation. I can't get her fear and screams out of my head. I knew what I was getting into when I took this job I just feel so helpless and sad. I have an appointment with our work grief counseling Monday. Anyways thanks for listening. Just needed to tell someone.,False,I'm a 911 dispatcher. Last night I heard a murder and I feel really messed up.
 # AFTER  -> As the title says I dispatch for 911 Last night I got a call for a domestic disturbance The woman calling was so scared She kept saying he s gonna kill me he s gonna fucking kill me I went through my protocol and tried to keep her calm Dispatchers have a system called ProQA that gives guidance on what to do in certain situations I followed that and had the police and Ambulance en route as soon as possible I tried to tell her to get to a safe place and kept assuring her help was on the way She said she had locked herself in the bathroom I could hear him pounding on the door yelling you re dead I m gonna fucking kill you I did everything I could I tried to keep her calm and help her but she was trapped I told her I was here for her and she wasn t alone She told me about her child a 4 year old girl and that she wasn t going to leave her Time seemed to drag on I checked with my officers who were only 2 minutes out and I felt some relief I just knew she was going to be okay Until I heard the sound of the door being kicked down All she said was God help me and the most haunting scream I have ever heard and dropped the phone 3 shots followed and I never heard from her again My crews arrived on scene and the husband was detained She was dead One shot to the head two to the chest I cannot stop replaying our conversation I can t get her fear and screams out of my head I knew what I was getting into when I took this job I just feel so helpless and sad I have an appointment with our work grief counseling Monday Anyways thanks for listening Just needed to tell someone ,False,I'm a 911 dispatcher. Last night I heard a murder and I feel really messed up.
