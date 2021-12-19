19d3b65930643e37a6f6ad08baf9c7c02e876c6f
Lenin Mookiah <leninmookiah@Lenins-MacBook-Pro.local>
2021-12-19 00:49:55 -0600

    stop words applied

--- Chapter_2/data_preproessing/bag_of_words_and_tf_idf.py
+++ Chapter_2/data_preproessing/bag_of_words_and_tf_idf.py
@@ -3,14 +3,18 @@
 # How to create term-frequency and inverse document frequency (tf-idf)
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
 import nltk
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import re
 import traceback
 from nltk.corpus import stopwords
-nltk.download('punkt')
 
+nltk.download('punkt')
+nltk.download('stopwords')
+print(stopwords.words('english'))
+# create a set of stop words
+stop_words = set(stopwords.words('english'))
 # list contains all the research interest from all the authors
 lst_research_interest = []
 
@@ -36,13 +40,15 @@ def create_bow_with_nltk(in_file, out_file):
             curr_research_interest = str(row['research_interest']).replace("##", " ").replace("_", " ")
             lst_research_interest.append(curr_research_interest)
             # TODO: add stop words filtering before doing tokenization
+            # word tokenize.
             curr_research_int_token = word_tokenize(curr_research_interest)
-            curr_line = f"{curr_authorname},{curr_email},{curr_affiliation},{curr_coauthors_names},{curr_research_int_token}"
+            curr_filtered_research = [w for w in curr_research_int_token if not w.lower() in stop_words]
+            curr_line = f"{curr_authorname},{curr_email},{curr_affiliation},{curr_coauthors_names},{curr_filtered_research}"
             # write to csv file
             f.write(curr_line + "\n")
         # close the output file object
         f.close()
-    except Exception as e:
+    except:
         traceback.print_exc()
 
 
@@ -53,11 +59,11 @@ def create_tf_idf_with_scikit():
         # use the td-idf instance to fit list of research_interest (feature)
         tfidf = tfidf_vectorizer.fit_transform(lst_research_interest)
         # tfidf[0].T.todense() provides the tf-idf dense vector calculated for the research_interest
-        df = pd.DataFrame(tfidf[0].T.todense(), index=tfidf_vectorizer.get_feature_names(), columns=["tf-idf"])
+        df = pd.DataFrame(tfidf[0].T.todense(), index=tfidf_vectorizer.get_feature_names_out(), columns=["tf-idf"])
         # sort the tf-idf calculated using `sort_values` of dataframe.
         df = df.sort_values('tf-idf', ascending=False)
         # top 30 words with highest tf-idf
-        print (df.head(30))
+        print(df.head(30))
     except:
         traceback.print_exc()
 
@@ -82,7 +88,19 @@ if __name__ == "__main__":
 # Term frequency is how common a word is, inverse document frequency (IDF) is how unique or rare a word is.
 # IDF(t) = log_e(Total number of documents / Number of documents with term t in it)
 ##############################################################################
-# ToDo List
+# Stop words
 ##############################################################################
-# ToDO: Also, apply the stemming concept with Bag of Words.
-#     (Stemming is the process of reducing a word to word stem.)
+# Mohamed M. E. A. Mahmoud,tntech.edu,Tennessee Tech University,nan,['security', 'and', 'privacy', 'preservation', 'for', 'wireless', 'networks']
+#  To
+# Mohamed M. E. A. Mahmoud,tntech.edu,Tennessee Tech University,nan,['security', 'privacy', 'preservation', 'wireless', 'networks']
+# Michael Chau,business.hku.hk,University of Hong Kong,nan,['information', 'systems', 'web', 'mining', 'data', 'mining', 'it', 'education']
+# To
+# Michael Chau,business.hku.hk,University of Hong Kong,nan,['information', 'systems', 'web', 'mining', 'data', 'mining', 'education']
+# Albert C. Gunther,wisc.edu,University of Wisconsin-Madison,Douglas Storey,['communication', 'journalism', 'mass', 'media', 'psychology', 'of', 'the', 'media', 'audience']
+# To
+# Albert C. Gunther,wisc.edu,University of Wisconsin-Madison,Douglas Storey,['communication', 'journalism', 'mass', 'media', 'psychology', 'media', 'audience']
+##############################################################################
+# Future improvement
+#############################################################################
+# - Also, apply the stemming concept with Bag of Words.
+#   (Stemming is the process of reducing a word to word stem.)
